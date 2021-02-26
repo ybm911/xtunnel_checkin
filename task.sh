f89c3599c90477862901ecfa866abdf5542a2f71
Elli0t <50075517+ybm911@users.noreply.github.com>
2021-02-26 19:47:20 +0800

    Update task.sh
    fix task.sh

--- task.sh
+++ task.sh
@@ -2,5 +2,5 @@
 RUN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
 echo $RUN_DIR
 CMD="python3 $RUN_DIR/run.py -c $RUN_DIR/config.yaml >> $RUN_DIR/run.log"
-echo "* 9 * * *   root    $CMD" > /etc/cron.d/xtunnel_checkin;
+echo "0 9 * * *   root    $CMD" > /etc/cron.d/xtunnel_checkin;
 /etc/init.d/cron reload;
